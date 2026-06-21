07ef762c174ab10798c7f44fd0588b4ae093f221
manishaalugolu-coder <manishaalugolu@gmail.com>
2026-06-21 09:01:42 +0530

    Initialize Firebase and Firestore in firebase.js

---
+++ firebase.js
@@ -0,0 +1,24 @@
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs
+} from
+"https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCxRX9gAx9zkL3P3K7rK9QKfsgtRufZA_E",
+  authDomain: "birthday-website-85b3e.firebaseapp.com",
+  projectId: "birthday-website-85b3e",
+  storageBucket: "birthday-website-85b3e.firebasestorage.app",
+  messagingSenderId: "1009777099486",
+  appId: "G-X5Z7Z49V64"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db, collection, addDoc, getDocs };
